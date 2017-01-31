096719d714ca6f6767ac50b6424ccca28b4e1382
Ichabond <tom@strickx.com>
2017-01-31 16:21:24 +0100

    Added support for AES-128 encrypted streams
    If an encrypted stream is detected, it will decode the chunks while writing them to disk.

--- hls_fetch.py
+++ hls_fetch.py
@@ -8,6 +8,13 @@ import argparse
 import os
 import posixpath
 import urllib.parse
+import re
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+
+def is_url(uri):
+    return re.match(r'https?://', uri) is not None
 
 
 class DownloadSegment(threading.Thread):
@@ -25,36 +32,53 @@ class DownloadSegment(threading.Thread):
             self.downloadQueue.task_done()
 
     def execute(self, item):
-        r = requests.get(item[1]+"/"+item[2], stream=True)
+        if item[1]:
+            url = item[1] + "/" + item[2]
+        else:
+            url = item[2]
+            item[2] = os.path.basename(urllib.parse.urlparse(url).path)
+        if item[3]:
+            backend = default_backend()
+            r = requests.get(item[3].uri)
+            key = r.content
+            cipher = Cipher(algorithms.AES(key), modes.CBC(bytes.fromhex(item[3].iv[2:])), backend=backend)
+            decryptor = cipher.decryptor()
+        r = requests.get(url, stream=True)
         with open(os.path.join(self.location, item[2]), 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
-                    f.write(chunk)
+                    if item[3]:
+                        f.write(decryptor.update(chunk))
+                    else:
+                        f.write(chunk)
 
 
-def highest_bandwidth(m3u8_obj):
-    highest_bw_playlist = {}
+def highest_bandwidth(m3u8_obj, location):
+    highest_bw_playlist = None
     for playlist in m3u8_obj.playlists:
         if not highest_bw_playlist:
             highest_bw_playlist = playlist
         elif playlist.stream_info.bandwidth > highest_bw_playlist.stream_info.bandwidth:
             highest_bw_playlist = playlist
+    if not is_url(highest_bw_playlist.uri):
+        highest_bw_playlist.base_uri = location
     return highest_bw_playlist
 
 
 def merge_files(filelist, source, destination, name):
     with open(os.path.join(destination, name), 'wb') as outfile:
         for file in filelist:
-            with open(os.path.join(source, file), 'rb') as readfile:
+            if is_url(file.uri):
+                uri = os.path.basename(urllib.parse.urlparse(file.uri).path)
+            else:
+                uri = file.uri
+            with open(os.path.join(source, uri), 'rb') as readfile:
                 shutil.copyfileobj(readfile, outfile)
 
 
 def m3u8_load(uri):
     r = requests.get(uri)
-    parsed_url = urllib.parse.urlparse(uri)
-    prefix = parsed_url.scheme + '://' + parsed_url.netloc
-    base_path = posixpath.normpath(parsed_url.path + '/..')
-    m3u8_obj = m3u8.M3U8(r.text, base_uri=urllib.parse.urljoin(prefix, base_path))
+    m3u8_obj = m3u8.M3U8(r.text)
     return m3u8_obj
 
 
@@ -63,11 +87,17 @@ def hls_fetch(playlist_location, storage_location, name="video.ts", threads=5):
     with tempfile.TemporaryDirectory() as download_location:
         num_worker_threads = threads
         playlist = m3u8_load(playlist_location)
-        high_bw = highest_bandwidth(playlist)
+        high_bw = highest_bandwidth(playlist, playlist_location)
         playlist = m3u8_load(high_bw.absolute_uri)
+        parsed_url = urllib.parse.urlparse(playlist_location)
+        prefix = parsed_url.scheme + '://' + parsed_url.netloc
+        base_path = posixpath.normpath(parsed_url.path + '/..')
+        base_uri = urllib.parse.urljoin(prefix, base_path)
         pool = list()
-        for number, file in enumerate(playlist.files):
-            download_queue.put([number, playlist.base_uri, file])
+        for number, file in enumerate(playlist.segments):
+            if not is_url(file.uri):
+                playlist.base_uri = base_uri
+            download_queue.put([number, playlist.base_uri, file.uri, file.key])
         for i in range(num_worker_threads):
             thread = DownloadSegment(download_queue, download_location)
             thread.daemon = True
@@ -78,7 +108,7 @@ def hls_fetch(playlist_location, storage_location, name="video.ts", threads=5):
             download_queue.put(None)
         for thread in pool:
             thread.join()
-        merge_files(playlist.files, download_location, storage_location, name)
+        merge_files(playlist.segments, download_location, storage_location, name)
 
 
 if __name__ == "__main__":
